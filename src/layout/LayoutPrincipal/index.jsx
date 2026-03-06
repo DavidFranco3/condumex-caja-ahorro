3b51283d6cbab6d00d9b168699eee9e5afeb19ca
David Franco <josedavidayalafranco3@gmail.com>
2026-03-06 16:14:30 -0600

    .

--- src/layout/LayoutPrincipal/index.jsx
+++ src/layout/LayoutPrincipal/index.jsx
@@ -147,10 +147,14 @@ function LayoutPrincipal(props) {
                   {/* Profile dropdown */}
                   <Menu as='div' className='ml-3 relative'>
                     <div>
-                      <MenuButton className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
+                      <MenuButton
+                        className='bg-transparent flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+                        style={{ borderRadius: '50%', padding: 0, border: 'none' }}
+                      >
                         <span className='sr-only'>Open user menu</span>
                         <img
                           className='h-8 w-8 rounded-full'
+                          style={{ borderRadius: '50%' }}
                           src={ImagenPerfil}
                           alt=''
                         />
@@ -165,7 +169,7 @@ function LayoutPrincipal(props) {
                       leaveFrom='transform opacity-100 scale-100'
                       leaveTo='transform opacity-0 scale-95'
                     >
-                      <MenuItems className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                      <MenuItems className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50'>
                         <MenuItem>
                           {({ focus }) => (
                             <button
